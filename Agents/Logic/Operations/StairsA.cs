30abb732b43bd99b5f503af5069a1415f9930f14
cckamilo <74384945+cckamilo@users.noreply.github.com>
2023-03-22 08:46:16 -0500

    Nueva version con ajustes de nombramiento de metodos

--- Agents/Logic/Operations/StairsA.cs
+++ Agents/Logic/Operations/StairsA.cs
@@ -8,9 +8,13 @@ namespace Agents.Logic.Operations
 		public StairsA(int value)
 		{
             
-            for (int stairNumber = 1; stairNumber <= value; stairNumber++)
+            for (int stairNumber = 0; stairNumber < value; stairNumber++)
             {
-                for (int i = 1; i <= stairNumber; i++)
+                for (int i = 0; i <= (value - stairNumber - 1); i++)
+                {
+                    Console.Write(" ");
+                }
+                for (int i = 0; i <= stairNumber; i++)
                 {
                    Console.Write("*");
                 }
--- Agents/Logic/Operations/StairsB.cs
+++ Agents/Logic/Operations/StairsB.cs
@@ -3,19 +3,25 @@ using Agents.Logic.Interfaces;
 
 namespace Agents.Logic.Operations
 {
-	public class StairsB: Response
-	{
-		public StairsB( int value)
-		{
-			  for (int stairNumber = 1; stairNumber <= value; stairNumber++)
+    public class StairsB : Response
+    {
+        public StairsB(int value)
+        {
+            for (int stairNumber = value; stairNumber >= 0; stairNumber--)
             {
-                for (int i = 1; i <= stairNumber; i++)
+
+                for (int i = 0; i <= (value - stairNumber - 1 ) ; i++)
                 {
-                   Console.Write("*");
+                    Console.Write(" ");
+                }
+                for (int i = 0; i < stairNumber; i++)
+                {
+                    Console.Write("*");
+
                 }
                 Console.WriteLine("");
             }
-		}
-	}
+        }
+    }
 }
 
