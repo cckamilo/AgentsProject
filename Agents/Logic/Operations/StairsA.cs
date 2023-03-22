c778981ff59906db030f6487bcc30ad3637fb30f
cckamilo <74384945+cckamilo@users.noreply.github.com>
2023-03-22 12:27:31 -0500

    Cometarios para algunos metodos

--- Agents/Logic/Operations/StairsA.cs
+++ Agents/Logic/Operations/StairsA.cs
@@ -8,13 +8,13 @@ namespace Agents.Logic.Operations
 		public StairsA(int value)
 		{
             
-            for (int stairNumber = 0; stairNumber < value; stairNumber++)
+            for (int stairNumber = 1; stairNumber <= value; stairNumber++)
             {
-                for (int i = 0; i <= (value - stairNumber - 1); i++)
+                for (int i = 1; i <= value - stairNumber; i++)
                 {
                     Console.Write(" ");
                 }
-                for (int i = 0; i <= stairNumber; i++)
+                for (int i = 1; i <= stairNumber; i++)
                 {
                    Console.Write("*");
                 }
--- Agents/Logic/Operations/StairsC.cs
+++ Agents/Logic/Operations/StairsC.cs
@@ -7,9 +7,39 @@ namespace Agents.Logic.Operations
 	{
 		public StairsC(int value)
 		{
-            for (int stairNumber = 1; stairNumber <= value; stairNumber++)
+
+            //Arriba
+            //Se agrega una nueva linea 
+            for (int stairNumber = 1; stairNumber <= value + 1; stairNumber++)
             {
-                for (int i = 1; i <= stairNumber; i++)
+                for (int i = 1; i <= (value + 1) - stairNumber; i++)
+                {
+                    Console.Write(" ");
+                }
+                for (int i = 1; i <= stairNumber ; i++)
+                {
+                    Console.Write("*");
+                }
+                for (int i = 0; i < stairNumber - 1; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+
+            //Abajo
+            for (int stairNumber = value; stairNumber > 0; stairNumber--)
+            {
+                for (int i = 0; i <= (value - stairNumber); i++)
+                {
+                    Console.Write(" ");
+                }
+                for (int i = 0; i < stairNumber; i++)
+                {
+                    Console.Write("*");
+
+                }
+                for (int i = 0; i < stairNumber - 1; i++)
                 {
                     Console.Write("*");
                 }
